94734c2b03e892bf034c3e59b1274f4505955844
rodriveiga01 <rodriveiga01@gmail.com>
2024-08-27 14:13:01 +0100

    Hero section

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,11 +3,11 @@ import logo from '/src/assets/logo_filled.png'
 export function Header() {
     return ( 
         <header>
-            <nav className="dark:bg-[#142c2c] px-4 lg:px-6 py-2.5">
+            <nav className="bg-[#142c2c] px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <a href="https://flowbite.com" className="flex items-center">
-                        <img src={logo} className="mr-3 h-6 sm:h-[68px]" alt="QuickCoords Logo" />
-                        <span className="self-center text-[36px] font-semibold whitespace-nowrap dark:text-[#c7e6e6]">QuickCoords</span>
+                        <img src={logo} className="mr-3 h-6 sm:h-16" alt="QuickCoords Logo" />
+                        <span className="self-center text-3xl font-semibold whitespace-nowrap dark:text-[#c7e6e6]">QuickCoords</span>
                     </a>
                     {/* TODO: add links to pages */}
                 </div>
