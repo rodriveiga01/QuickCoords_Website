58b93a80ab8d5306ddbcbd44f69f5129e34a0663
rodriveiga01 <rodriveiga01@gmail.com>
2024-08-30 10:40:20 +0100

    Google analytics setup (test)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePluginRadar } from 'vite-plugin-radar';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-LY6NPPFJEE',
+      },
+    }),
+  ],
   base: '/quickCoords_website/',
-})
+});
